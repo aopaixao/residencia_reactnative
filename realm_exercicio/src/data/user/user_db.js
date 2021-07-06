a8b2db4e9cab01af91a8ebc0ec0945ae0099df90
aopaixao <aopaixao@outlook.com>
2021-07-06 08:03:31 -0300

    Implementação do auto-incremento no id

--- realm_exercicio/src/data/user/user_db.js
+++ realm_exercicio/src/data/user/user_db.js
@@ -23,9 +23,15 @@ let listUsers = () => {
 }
 
 let addUser = (_name, _email, _login, _password) => {
+
+  const lastUser = realm_user.objects('User').sorted('user_id', true)[0];
+  const highestId = lastUser == null ? 0 : lastUser.user_id;
+  const new_id = highestId == null ? 1 : highestId + 1;
+
   console.log(_name, _email, _login, _password);
   realm_user.write(() => {
     const user = realm_user.create('User', {
+      user_id: new_id,
       user_name: _name.user_name,
       user_email: _email.user_email,
       user_login: _login.user_login,
